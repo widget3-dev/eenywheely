19cda35df4cb59472120e99baebae0717ed0a182
widget3-dev <jaromir@widget3.dev>
2021-04-21 14:40:20 +0200

    minor improvements

--- lib/ui/wheely/wheely_view.dart
+++ lib/ui/wheely/wheely_view.dart
@@ -6,16 +6,19 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class WheelyView extends HookWidget {
+  static const kForce = 3.8; // between 1 and 10
+  static const kRotationsPerForce = 5; //between 1 and 10
+
   Map<int, Widget> _buildWheelItemMap(
       List<String> items, double radius, double radian) {
     Map<int, Widget> wheelItemMap = {};
-    items.asMap().forEach((key, item) {
-      wheelItemMap[key] = CustomPaint(
+    items.asMap().forEach((index, item) {
+      wheelItemMap[index] = CustomPaint(
         painter: ItemPainter(
           text: item,
           radius: radius,
           radian: radian,
-          color: key.isOdd ? Colors.red[800] : Colors.grey[800],
+          color: index.isOdd ? Colors.red[800] : Colors.grey[800],
         ),
       );
     });
@@ -26,15 +29,21 @@ class WheelyView extends HookWidget {
       Map<int, Widget> itemMap, Animation<double> animation, double radian) {
     List<Widget> animatedItems = [];
 
-    itemMap.forEach((key, item) {
+    itemMap.forEach((index, item) {
       animatedItems.add(AnimatedWheelItem(
         animation: animation,
-        angle: (key * radian),
+        angle: (index * radian),
         child: item,
       ));
     });
 
     return animatedItems;
+  }
+
+  void _startWheel(AnimationController controller) {
+    controller
+      ..reset()
+      ..forward();
   }
 
   @override
@@ -57,22 +66,31 @@ class WheelyView extends HookWidget {
     final animatedWheelItems =
         _buildAnimatedWheelItems(wheelItems, animation, radian);
 
-    return Container(
-      height: diameter,
-      width: diameter,
-      child: Stack(
-        children: [
-          ...animatedWheelItems,
-          Align(
-            alignment: Alignment.centerRight,
-            child: Container(
-              height: 12.0,
-              width: 12.0,
-              decoration:
-                  BoxDecoration(shape: BoxShape.circle, color: Colors.red),
-            ),
+    return Scaffold(
+      floatingActionButton: IconButton(
+        iconSize: 42.0,
+        icon: Icon(Icons.play_arrow_outlined),
+        onPressed: () => _startWheel(controller), //TODO FIX
+      ),
+      body: SafeArea(
+        child: Container(
+          height: diameter,
+          width: diameter,
+          child: Stack(
+            children: [
+              ...animatedWheelItems,
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  height: 12.0,
+                  width: 12.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.yellow[800]),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
